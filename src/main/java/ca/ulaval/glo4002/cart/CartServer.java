a0cd7832b848bc08721c47392eb441aabaa4af97
Jannik <jannik.levesque.1@ulaval.ca>
2018-11-21 19:12:01 -0500

    added Procfile

--- src/main/java/ca/ulaval/glo4002/cart/CartServer.java
+++ src/main/java/ca/ulaval/glo4002/cart/CartServer.java
@@ -20,7 +20,7 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 
 public class CartServer implements Runnable {
-    private static final int PORT = 7222;
+    private static final String PORT_PARAMETER = "port";
 
     public static void main(String[] args) {
         new CartServer().run();
@@ -36,7 +36,7 @@ public class CartServer implements Runnable {
     }
 
     private void startServer() {
-        Server server = new Server(PORT);
+        Server server = new Server(Integer.parseInt(System.getProperty(PORT_PARAMETER)));
         ServletContextHandler contextHandler = new ServletContextHandler(server, "/");
         contextHandler.addFilter(EntityManagerContextFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
 
